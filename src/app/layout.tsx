55aed36df4045f77db6e99f3f8dd4ca5b70d1943
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-04-25 10:39:18 +0200

    Add ReactQuery

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ArrowUpRight } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Logo } from "@/components/common/logo";
 import { Button } from "@/components/common/button";
+import { Provider } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,30 +29,32 @@ export default function RootLayout({
           inter.className
         )}
       >
-        <main className="">
-          <header className="h-20 px-8 flex justify-between items-center">
-            <Link href={"/"}>
-              <Logo />
-            </Link>
-            <div className="flex items-center gap-2 divide-x space-x-2 text-sm">
-              <div>
-                Time left to vote <span className="pl-2">10d : 5h : 12m</span>
+        <Provider>
+          <main className="">
+            <header className="h-20 px-8 flex justify-between items-center">
+              <Link href={"/"}>
+                <Logo />
+              </Link>
+              <div className="flex items-center gap-2 divide-x space-x-2 text-sm">
+                <div>
+                  Time left to vote <span className="pl-2">10d : 5h : 12m</span>
+                </div>
+                <Button
+                  icon={ArrowUpRight}
+                  iconSide="right"
+                  variant="link"
+                  className="pl-4"
+                >
+                  View badgeholder manual
+                </Button>
               </div>
-              <Button
-                icon={ArrowUpRight}
-                iconSide="right"
-                variant="link"
-                className="pl-4"
-              >
-                View badgeholder manual
-              </Button>
+              <Button variant="outline">todo.eth</Button>
+            </header>
+            <div className="flex gap-8 max-w-screen-lg mx-auto py-16">
+              {children}
             </div>
-            <Button variant="outline">todo.eth</Button>
-          </header>
-          <div className="flex gap-8 max-w-screen-lg mx-auto py-16">
-            {children}
-          </div>
-        </main>
+          </main>
+        </Provider>
       </body>
     </html>
   );
---
+++ src/app/providers.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export function Provider({ children }: PropsWithChildren) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
