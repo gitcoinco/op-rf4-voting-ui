82047c6857616b81b63cd115a7c94f3ed699f3e9
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-04-25 10:39:54 +0200

    Add Agora API proxy

---
+++ src/pages/api/agora/[...api].ts
@@ -0,0 +1,25 @@
+// Proxy requests to Agora API
+
+import { IncomingMessage, ServerResponse } from "http";
+import httpProxy from "http-proxy";
+
+const AGORA_API_URL = process.env.AGORA_API_URL;
+const AGORA_API_KEY = process.env.AGORA_API_KEY;
+
+const proxy = httpProxy.createProxyServer();
+
+proxy.on("proxyReq", function (req) {
+  req.setHeader("Authorization", `Bearer ${AGORA_API_KEY}`);
+});
+
+export const config = { api: { bodyParser: false } };
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  req.url = req.url?.replace("/api/agora", "");
+
+  return new Promise((resolve, reject) => {
+    proxy.web(req, res, { target: AGORA_API_URL, changeOrigin: true }, (err) =>
+      err ? reject(err) : resolve({})
+    );
+  });
+}
