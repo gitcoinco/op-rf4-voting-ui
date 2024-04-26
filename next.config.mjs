f45aa7e7099939c41be4a0c444b4b12cae437cb6
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-04-26 12:53:41 +0200

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding", {
+      "node-gyp-build": "commonjs node-gyp-build",
+    });
+    return config;
+  },
+
   images: {
     remotePatterns: [
       {
