620ab5023785dc98a6bdc08194f1b2528788f847
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-05-16 18:20:02 +0200

    Fix build

--- src/app/ballot/metrics/page.tsx
+++ src/app/ballot/metrics/page.tsx
@@ -1,12 +1,14 @@
+import { Suspense } from "react";
 import { MetricsList } from "../../../components/metrics-list";
-import { useFilter } from "@/hooks/useFilter";
 import { MetricsFilter } from "@/components/metrics-list/metrics-filter";
 
 export default function MetricsPage() {
+  // Suspense needed to build because of useFilter
+  // useSearchParams() should be wrapped in a suspense boundary at page "/ballot/metrics".
   return (
-    <>
+    <Suspense>
       <MetricsFilter />
       <MetricsList />
-    </>
+    </Suspense>
   );
 }
