84fa635a2278a93fee2e32dc10d3c6ed437a9f80
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-05-23 12:56:46 +0200

    Fix metric page sidebar

--- src/app/metrics/[id]/page.tsx
+++ src/app/metrics/[id]/page.tsx
@@ -65,7 +65,9 @@ export default function MetricDetailsPage({ params: { id = "" } }) {
         <Comments />
         <MetricPagination id={id} />
       </section>
-      <DistributionSidebar id={id} />
+      <aside>
+        <DistributionSidebar id={id} />
+      </aside>
     </>
   );
 }
