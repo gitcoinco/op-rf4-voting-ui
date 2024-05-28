dd83e5e042e8362bfba16ecebc3f5d93f8e5ba0c
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-05-28 08:31:50 +0200

    Fix pagination and breadcrumb links

--- src/app/metrics/[id]/page.tsx
+++ src/app/metrics/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { CheckCircle, MessageCircle, User } from "lucide-react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,39 +6,12 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { cn } from "@/lib/utils";
 import { Comments } from "@/components/metrics/comments";
 import { MetricPagination } from "@/components/metrics/metric-pagination";
 import { MetricDetails } from "@/components/metrics-details";
 import { DistributionSidebar } from "@/components/metrics/distribution-sidebar";
+import Link from "next/link";
 
-const badgeholderStats = [
-  {
-    label: "Viewed",
-    hint: "?",
-    value: "45 of 150",
-    icon: User,
-  },
-  {
-    label: "Added to ballots",
-    hint: "?",
-    value: "90%",
-    icon: ({ className = "" }) => (
-      <CheckCircle className={cn("text-green-500", className)} />
-    ),
-  },
-  {
-    label: "Comments",
-    value: "2",
-    icon: MessageCircle,
-  },
-];
-
-/*
-  <div className="">{children}</div>
-      <aside>
-      </aside>
-      */
 export default function MetricDetailsPage({ params: { id = "" } }) {
   return (
     <>
@@ -47,11 +19,15 @@ export default function MetricDetailsPage({ params: { id = "" } }) {
         <Breadcrumb className="mb-6">
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbLink href="/ballot">Ballot</BreadcrumbLink>
+              <BreadcrumbLink asChild>
+                <Link href="/ballot">Ballot</Link>
+              </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
-              <BreadcrumbLink href="/ballot/metrics">Metrics</BreadcrumbLink>
+              <BreadcrumbLink asChild>
+                <Link href="/ballot/metrics">Metrics</Link>
+              </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
