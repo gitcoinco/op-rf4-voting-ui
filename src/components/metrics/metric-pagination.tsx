3f6919e375518476e04e20726a6a27d796bf340f
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-05-29 20:16:38 +0200

    Improve metric pagination

--- src/components/metrics/metric-pagination.tsx
+++ src/components/metrics/metric-pagination.tsx
@@ -2,13 +2,13 @@
 import {
   Pagination,
   PaginationContent,
-  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { useMetricIds } from "@/hooks/useMetrics";
+import { cn } from "@/lib/utils";
 
 export function MetricPagination({ id = "" }) {
   const { data, isPending } = useMetricIds();
@@ -16,59 +16,36 @@ export function MetricPagination({ id = "" }) {
   const current = data?.findIndex((_id) => _id === id) ?? 0;
   const pages = data ?? [];
 
-  const maxPagesToShow = 5;
-  const totalPages = pages.length;
-  const startPage = Math.max(
-    0,
-    Math.min(current - 2, totalPages - maxPagesToShow)
-  );
-  const endPage = Math.min(startPage + maxPagesToShow, totalPages);
-
-  const visiblePages = pages.slice(startPage, endPage);
-
   return (
     <div className="fixed bottom-10 bg-white shadow border rounded-lg p-2 left-1/2 -translate-x-1/2">
       <Pagination>
         <PaginationContent>
-          {current > 0 && (
-            <PaginationItem>
-              <PaginationPrevious href={`/metrics/${pages[current - 1]}`} />
-            </PaginationItem>
-          )}
+          <PaginationItem
+            className={cn({
+              ["pointer-events-none opacity-50"]: current === 0,
+            })}
+          >
+            <PaginationPrevious href={`/metrics/${pages[current - 1]}`} />
+          </PaginationItem>
 
-          {visiblePages.map((pageId, index) => (
+          {pages.map((pageId, index) => (
             <PaginationItem key={pageId}>
               <PaginationLink
                 href={`/metrics/${pageId}`}
-                isActive={startPage + index === current}
+                isActive={index === current}
               >
-                {startPage + index + 1}
+                {index + 1}
               </PaginationLink>
             </PaginationItem>
           ))}
-          {endPage < totalPages && (
-            <>
-              {endPage < totalPages - 1 && (
-                <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-              )}
-              <PaginationItem>
-                <PaginationLink
-                  href={`/metrics/${pages[totalPages - 1]}`}
-                  isActive={current === totalPages - 1}
-                >
-                  {totalPages}
-                </PaginationLink>
-              </PaginationItem>
-            </>
-          )}
 
-          {current < totalPages - 1 && (
-            <PaginationItem>
-              <PaginationNext href={`/metrics/${pages[current + 1]}`} />
-            </PaginationItem>
-          )}
+          <PaginationItem
+            className={cn({
+              ["pointer-events-none opacity-50"]: current === pages.length - 1,
+            })}
+          >
+            <PaginationNext href={`/metrics/${pages[current + 1]}`} />
+          </PaginationItem>
         </PaginationContent>
       </Pagination>
     </div>
