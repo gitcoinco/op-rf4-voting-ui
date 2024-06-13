02631a953e24e743983cdfdbf3a30d44688044a1
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-13 08:03:37 +0200

    Fix lock icons

--- src/components/metrics-editor/index.tsx
+++ src/components/metrics-editor/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { NumericFormat } from "react-number-format";
-import { Lock, LockOpen, Minus, Plus, Trash2 } from "lucide-react";
+import { Minus, Plus, Trash2 } from "lucide-react";
 import { Heading } from "@/components/ui/headings";
 
 import { Button } from "@/components/ui/button";
@@ -69,9 +69,9 @@ export function MetricsEditor({
                 <div className="flex gap-2">
                   <Button
                     size={"icon"}
-                    variant="ghost"
-                    icon={locked ? Lock : LockOpen}
-                    className={cn("rounded-full", { ["opacity-50"]: !locked })}
+                    variant={locked ? "secondary" : "ghost"}
+                    icon={locked ? LockFillLocked : LockFillUnlocked}
+                    className={cn({ ["opacity-50"]: !locked })}
                     tabIndex={-1}
                     onClick={() => set(id, allocation, locked)}
                   />
@@ -122,7 +122,6 @@ export function MetricsEditor({
                   </div>
                   <Button
                     size="icon"
-                    className="rounded-full"
                     variant="ghost"
                     icon={Trash2}
                     tabIndex={-1}
@@ -136,3 +135,33 @@ export function MetricsEditor({
     </div>
   );
 }
+
+const LockFillLocked = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.6667 6.66732H13.3333C13.7015 6.66732 14 6.96578 14 7.33398V14.0007C14 14.3689 13.7015 14.6673 13.3333 14.6673H2.66667C2.29848 14.6673 2 14.3689 2 14.0007V7.33398C2 6.96578 2.29848 6.66732 2.66667 6.66732H3.33333V6.00065C3.33333 3.42332 5.42267 1.33398 8 1.33398C10.5773 1.33398 12.6667 3.42332 12.6667 6.00065V6.66732ZM11.3333 6.66732V6.00065C11.3333 4.1597 9.84093 2.66732 8 2.66732C6.15905 2.66732 4.66667 4.1597 4.66667 6.00065V6.66732H11.3333ZM7.33333 9.33398V12.0007H8.66667V9.33398H7.33333Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+const LockFillUnlocked = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.66667 6.66732H13.3333C13.7015 6.66732 14 6.96578 14 7.33398V14.0007C14 14.3689 13.7015 14.6673 13.3333 14.6673H2.66667C2.29848 14.6673 2 14.3689 2 14.0007V7.33398C2 6.96578 2.29848 6.66732 2.66667 6.66732H3.33333V6.00065C3.33333 3.42332 5.42267 1.33398 8 1.33398C9.827 1.33398 11.4087 2.38385 12.1749 3.9132L10.9821 4.50961C10.4348 3.41722 9.305 2.66732 8 2.66732C6.15905 2.66732 4.66667 4.1597 4.66667 6.00065V6.66732ZM6.66667 10.0007V11.334H9.33333V10.0007H6.66667Z"
+      fill="currentColor"
+    />
+  </svg>
+);
