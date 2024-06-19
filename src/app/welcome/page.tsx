1f237ca9be87035fd37bbe388385634df39b7e4b
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-19 11:57:39 +0200

    Update imapct metric count in welcome screen

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -8,29 +8,32 @@ import onboard2 from "../../../public/onboard2.svg";
 import onboard3 from "../../../public/onboard3.svg";
 import { useAccount } from "wagmi";
 import { redirect } from "next/navigation";
+import { useMetricIds } from "@/hooks/useMetrics";
 
-const slides = [
-  {
-    title: "Welcome to metrics based voting",
-    description:
-      "In this round, you’ll be voting on metrics that you think matter most when evaluating the impact of onchain projects across the Superchain.",
-    image: onboard1,
-  },
-  {
-    title: "You’ll choose metrics, not projects",
-    description:
-      "Learn about each metric, add comments for your fellow badgeholders, and fill your ballot. Remember, you’ll be choosing metrics, not projects.",
-    image: onboard2,
-  },
-  {
-    title: "How the metrics were created",
-    description:
-      "The list of 20 metrics for this round of voting were created by the Optimism Foundation in partnership with our citizen badgeholders.",
-    image: onboard3,
-  },
-];
 export default function Welcome() {
   const { address } = useAccount();
+  const { data: metricCount } = useMetricIds();
+  const slides = [
+    {
+      title: "Welcome to metrics based voting",
+      description:
+        "In this round, you’ll be voting on metrics that you think matter most when evaluating the impact of onchain projects across the Superchain.",
+      image: onboard1,
+    },
+    {
+      title: "You’ll choose metrics, not projects",
+      description:
+        "Learn about each metric, add comments for your fellow badgeholders, and fill your ballot. Remember, you’ll be choosing metrics, not projects.",
+      image: onboard2,
+    },
+    {
+      title: "How the metrics were created",
+      description: `The list of ${
+        metricCount?.length ?? 12
+      } metrics for this round of voting were created by the Optimism Foundation in partnership with our citizen badgeholders.`,
+      image: onboard3,
+    },
+  ];
   if (!address) {
     return redirect("/");
   }
