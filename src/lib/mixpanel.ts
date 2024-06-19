45d7306b591c22f75230c2cc9e6f583ed2c3903d
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-19 13:50:07 +0200

    Fix build

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -1,6 +1,6 @@
 import mixpanel from "mixpanel-browser";
 
-mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_API, {
+mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_API!, {
   debug: true,
   track_pageview: true,
   persistence: "localStorage",
