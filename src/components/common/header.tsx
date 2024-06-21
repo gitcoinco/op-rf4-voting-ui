285b03887c1390a80f0be32244161324cbf5f717
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-21 07:16:42 +0200

    Add external badgeholder manual link track

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
 import { Logo } from "@/components/common/logo";
@@ -7,6 +9,7 @@ import { ConnectButton } from "../auth/connect-button";
 import { SignMessage } from "../auth/sign-message";
 import { VotingEndsIn } from "../voting-ends-in";
 import { votingEndDate, badgeholderManualUrl } from "@/config";
+import mixpanel from "mixpanel-browser";
 
 export function Header() {
   return (
@@ -20,7 +23,12 @@ export function Header() {
           <VotingEndsIn className="pl-2" date={votingEndDate} />
         </div>
         <Link href={badgeholderManualUrl} target="_blank">
-          <Button iconRight={ArrowUpRight} variant="link" className="pl-4">
+          <Button
+            iconRight={ArrowUpRight}
+            variant="link"
+            className="pl-4"
+            onClick={() => mixpanel.track("Open Manual", { external: true })}
+          >
             View badgeholder manual
           </Button>
         </Link>
