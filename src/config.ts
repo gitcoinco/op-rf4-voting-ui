2dd8dc0b7baf9e1fc3d0a96592cebb667e9bb2b9
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-25 17:40:17 +0200

    Update votingEndDate

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const pageTitle = "OP RetroFunding Round 4";
 export const agoraRoundsAPI = "/api/agora/retrofunding/rounds/4";
 
-export const votingEndDate = new Date("2024-07-31");
+export const votingEndDate = new Date(Date.UTC(2024, 6, 11, 19, 0, 0));
 
 export const badgeholderManualUrl =
   "https://gov.optimism.io/t/retro-funding-4-badgeholder-manual/8239";
